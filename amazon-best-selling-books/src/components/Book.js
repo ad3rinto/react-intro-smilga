9af1134b5149d1052057aa1ee0692e26498a8508
ad3rinto <aad3rinto@gmail.com>
2023-02-21 10:53:15 +0000

    remove redundant input and button

--- amazon-best-selling-books/src/components/Book.js
+++ amazon-best-selling-books/src/components/Book.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import EventsExamples from './EventsExamples';
+
 
 
 function Book(props) {
-    const getBook = (id) => {
-        const book = props.books.find((book) => book.id === id)
-        console.log(book);
+    // const getBook = (id) => {
+    //     const book = props.books.find((book) => book.id === id)
+    //     console.log(book);
 
-    }
+    // }
 
-    const getSingleBook = () => {
-        getBook(props.id)
-    }
+    // const getSingleBook = () => {
+    //     getBook(props.id)
+    // }
 
 
 
     return (
 
         <article className='book'>
-            <EventsExamples title={props.title} getBook={getBook} getSingleBook={getSingleBook} />
+
             <img src={props.img} alt="bookies" />
             <h2>{props.title}</h2>
             <h4>{props.author}</h4>
