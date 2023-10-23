0ccf0d1ff1568042f674f68ce57289824da7be7e
ad3rinto <aad3rinto@gmail.com>
2023-10-23 11:38:50 +0100

    final coffee machine oop exercise

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -2,5 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-my_coffee_maker = CoffeeMaker()
-print(my_coffee_maker)
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()   
+menu = Menu()
+# menu_item = MenuItem()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like to order? ({options}): \n").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+        
