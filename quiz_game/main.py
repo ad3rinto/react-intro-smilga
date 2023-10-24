bdec5711142928fa667d2299020536c505e86869
ad3rinto <aad3rinto@gmail.com>
2023-10-24 15:34:36 +0100

    solved the OOP quiz challenge

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,5 +1,7 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+is_on = True
 
 question_bank = []
 length_of_questions = len(question_data)
@@ -10,3 +12,10 @@ for item in range(length_of_questions):
     question_bank.append(q_data)
 
 
+new_quiz = QuizBrain(question_bank)
+
+while new_quiz.still_has_question():
+    new_quiz.next_question(question_bank)
+
+print("You have completed the quiz")
+print(f"Your final score is {new_quiz.score}/{len(question_bank)}")
