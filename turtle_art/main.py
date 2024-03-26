5e75a79e886b61546e6847af9f1f3645f95b457a
ad3rinto <aad3rinto@gmail.com>
2024-03-26 16:02:43 +0000

    NFI

--- turtle_art/main.py
+++ turtle_art/main.py
@@ -1,31 +1,14 @@
 from turtle import Turtle, Screen
 
-
 tim = Turtle()
 tim.color("red")
 tim.shape("turtle")
 
 for i in range(40):
-    tim.pendown(10)
+    tim.pendown()
     tim.forward(10)
-    tim.pendown(10)
+    tim.pendown()
     tim.forward(10)
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 screen = Screen()
 screen.exitonclick()
-
